45178592e35badec2bb12969e68f5b83a28ed5b7
sankalpaneupane <sneupane@hamropatro.com>
2024-05-08 06:27:03 +0545

    feat: add onboarding with lottie animations

--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -1,83 +1,43 @@
-import {View, Text, ImageBackground, Image, StyleSheet} from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context";
-import {ButtonComponent} from "../reusable/ButtonComponent";
-
-
-const LandingScreen = ({navigation}) => {
+import {StyleSheet} from "react-native";
+import Onboarding from 'react-native-onboarding-swiper';
+import LottieView from 'lottie-react-native';
+import {useNavigation} from "@react-navigation/native";
+const LandingScreen = () => {
+    const navigation = useNavigation();
     return (
-        <ImageBackground
-            source={require("../assets/splash.jpeg")}
-            style={styles.backgroundImage}
-        >
-            <SafeAreaView style={styles.overlay}>
-                <View style={styles.logoContainer}>
-                    <Image
-                        source={require('../assets/logo.png')}
-                        style={styles.logo}
-                        />
-                    <Text style={styles.logoText}>100K+ Premium Recipe</Text>
-                </View>
-                <View style={styles.bottomHalf}>
-                    <View style={styles.textContainer}>
-                        <Text style={styles.heading}>Get</Text>
-                        <Text style={styles.heading}>Cooking</Text>
-                        <Text style={styles.smallText}>Simple way to find tasty recipe</Text>
-                    </View>
-                    <ButtonComponent onPress={()=>navigation.navigate("Login")} title='Start Cooking'/>
-                </View>
-            </SafeAreaView>
-        </ImageBackground>
-  );
+        <Onboarding
+            onSkip={() => navigation.navigate('Home')}
+            onDone={() => {navigation.navigate('Home')}}
+            pages={[
+                {
+                    backgroundColor: "#fff",
+                    image:  <LottieView style={styles.image} source={require('../assets/animations/landingImage1.json')} autoPlay loop />,
+                    title: 'Culinary Delights',
+                    subtitle: 'Transform ordinary ingredients into extraordinary meals',
+                },
+                {
+                    backgroundColor: "#fff",
+                    image:  <LottieView style={styles.image} source={require('../assets/animations/greenAnimation1.json')} autoPlay loop />,
+                    title: 'Kitchen Artistry',
+                    subtitle: 'Discover mouthwatering recipes from around the world',
+                },
+                {
+                    backgroundColor: "#fff",
+                    image:  <LottieView style={styles.image} source={require('../assets/animations/greenAnimation2.json')} autoPlay loop />,
+                    title: 'Flavor Odyssey',
+                    subtitle: 'Transform ordinary ingredients into extraordinary meals',
+                },
+            ]}
+        />
+    )
 }
 
 const styles = StyleSheet.create({
-
-    backgroundImage: {
-            flex: 1,
-            resizeMode: "cover",
-            justifyContent: "center",
-    },
-    overlay: {
-            flex: 1,
-            backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent black overlay
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            paddingVertical: 100
-    },
-    logoContainer: {
-            alignItems: 'center',
-    },
-    logo: {
-             width: 79,
-            height: 79,
-    },
-    logoText: {
-            color: 'white',
-            fontSize: 18,
-            fontWeight: 600,
-            marginTop: 10,
-    },
-    bottomHalf : {
-
-    },
-    textContainer: {
-
-    },
-    heading: {
-            color: 'white',
-            fontSize: 50,
-            fontWeight: 'bold',
-            marginBottom: 10,
-            textAlign: 'center'
-    },
-    smallText: {
-            color: 'white',
-            fontSize: 16,
-            textAlign: 'center',
-            fontWeight: 400,
-        marginTop: 20
+    image: {
+        width: '100%',
+        height: 300,
+        borderRadius: 100,
     }
-
 })
 
-export default LandingScreen;
+export default LandingScreen
